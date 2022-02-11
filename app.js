763855064307b0f100fd3ac6214227835ea3d9e7
JAYAKRISHNAN KP <kpjayakrishnan01@gmail.com>
2022-02-11 15:25:36 +0530

    Validation & Alert

--- app.js
+++ app.js
@@ -24,6 +24,31 @@ UI.prototype.addBookToList = function(book){
     list.appendChild(row);
 }
 
+// show alert
+UI.prototype.showAlert = function(message, className) {
+    // create div
+    const div = document.createElement('div');
+    // add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+
+    // Get parent
+    const container = document.querySelector('.container');
+    // Get form
+    const form = document.querySelector('#book-form');
+    // Insert alert
+    container.insertBefore(div, form);
+
+    // Timeout after 3 sec
+    setTimeout(function(){
+        document.querySelector('.alert')
+        .remove();
+    },3000);
+
+}
+
+
 // Clear Fields
 UI.prototype.clearFields = function () {
    
@@ -47,11 +72,23 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     // Instantiate UI
     const ui = new UI();
 
-    // Add book to list
-    ui.addBookToList(book);
+    // Validate
+    if(title === '' || author === '' || isbn === '') {
+        // Error alert
+        ui.showAlert('please fill in the all fields ' , 'error');
+    } else {
+         // Add book to list
+         ui.addBookToList(book);
 
-    // Clear fields
-    ui.clearFields();
+        //  show success
+        ui.showAlert('Book Added ! ' , 'success');
+
+         // Clear fields
+         ui.clearFields();
+
+    }
+
+   
 
     e.preventDefault();
 });
