7fdb6e1b12394df4b77f1ee780f405c806b59760
JAYAKRISHNAN KP <kpjayakrishnan01@gmail.com>
2022-02-11 20:47:19 +0530

    Delete Book From List

--- app.js
+++ app.js
@@ -47,6 +47,13 @@ UI.prototype.showAlert = function(message, className) {
     },3000);
 
 }
+// Delete Book
+UI.prototype.deleteBook = function(target) {
+    if(target.className === 'delete') {
+        target.parentElement.parentElement.remove();
+    }
+}
+
 
 
 // Clear Fields
@@ -59,7 +66,7 @@ UI.prototype.clearFields = function () {
 }
 
 
-// Event Listeners
+// Event Listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
     // Get form values
     const title = document.getElementById('title').value,
@@ -92,3 +99,19 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 
     e.preventDefault();
 });
+
+
+// Event Listeners for delete
+document.getElementById('book-list').addEventListener('click', function(e){
+    
+    // Instantiate UI
+     const ui = new UI();
+
+    // Delete book
+    ui.deleteBook(e.target);
+
+    // show message
+    ui.showAlert('Book Removed ! ' , 'success');
+
+    e.preventDefault()
+})
