a4767a6478725c1318b1efb0bf87ef7e11edc876
JAYAKRISHNAN KP <kpjayakrishnan01@gmail.com>
2022-02-12 12:06:18 +0530

    Add  Local storage

--- appes6.js
+++ appes6.js
@@ -59,6 +59,57 @@ class UI {
     }
 }
 
+// Local Storage Class
+class Store {
+
+        static getBooks() {
+            let books;
+            if(localStorage.getItem('books') === null) {
+              books = [];
+            } else {
+              books = JSON.parse(localStorage.getItem('books'));
+            }
+        
+            return books;
+    }
+    static displayBooks() {
+        const books = Store.getBooks();
+
+    books.forEach(function(book){
+      const ui  = new UI;
+
+      // Add book to UI
+      ui.addBookToList(book);
+    });
+
+    }
+    static addBook(book) {
+
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+    static removeBook(isbn) {
+
+        
+        const books = Store.getBooks();
+
+        books.forEach(function(book, index){
+         if(book.isbn === isbn) {
+          books.splice(index, 1);
+         }
+        });
+    
+        localStorage.setItem('books', JSON.stringify(books));
+      }
+}
+
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 
 // Event Listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
@@ -72,6 +123,8 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 
     // Instantiate UI
     const ui = new UI();
+    console.log(ui);
+  
 
     // Validate
     if(title === '' || author === '' || isbn === '') {
@@ -80,6 +133,9 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     } else {
          // Add book to list
          ui.addBookToList(book);
+
+        //  Add to LS
+        Store.addBook(book);
 
         //  show success
         ui.showAlert('Book Added ! ' , 'success');
@@ -103,6 +159,9 @@ document.getElementById('book-list').addEventListener('click', function(e){
 
     // Delete book
     ui.deleteBook(e.target);
+
+    // Remove from LS
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     // show message
     ui.showAlert('Book Removed ! ' , 'success');
